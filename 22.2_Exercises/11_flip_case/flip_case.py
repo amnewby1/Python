ba92defb8bffe6b1f84d60416e89963dcfeecf6f
A_Simms <114841649+amnewby1@users.noreply.github.com>
2023-03-18 21:43:33 -0400

    Add files via upload

---
+++ 22.2_Exercises/11_flip_case/flip_case.py
@@ -0,0 +1,23 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    to_swap = to_swap.lower()
+    completed_swap = ""
+
+    for char in phrase:
+        if char.lower() == to_swap:
+            char = char.swapcase()
+        completed_swap += char
+
+    return completed_swap
+    
